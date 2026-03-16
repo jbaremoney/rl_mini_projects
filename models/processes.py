6f47e5c0692c18aae93d697619e59dcfb4b45b1c
jbaremoney <114310093+jbaremoney@users.noreply.github.com>
2026-03-16 11:55:37 -0500

    working MC simulation for reactor

--- models/processes.py
+++ models/processes.py
@@ -5,15 +5,23 @@ from scipy.sparse.linalg import gmres
 
 
 class FiniteMarkovProcess(ABC):
-    # TODO: add optional initial state distribution, key value for state indices
-    def __init__(self, states: list):
+    # TODO: GENERALIZE INITIAL STATE AND INITIAL STATE DISTRIBUTION TO ONE OBJECT, fix initializing/current state
+    def __init__(self, states: list, trans_kernel=None, initial_state=None, initial_dist=None):
         self.rng = np.random.default_rng()
-
-        self.states = states
-
+        self.states = states # keeping as list just lets us access by index
         self.n = len(states)
-
         self.state_to_idx = {s: i for i, s in enumerate(states)}
+        self.initial_state = initial_state
+        if trans_kernel is not None:
+            self.trans_kernel = trans_kernel
+        else:
+            pass
+        # only need to define initial dist if no initial state
+        if self.initial_state is None:
+            if initial_dist is None:
+                self.initial_dist = [1/self.n for i in range(self.n)] # uniform
+            else:
+                self.initial_dist = initial_dist
 
     @abstractmethod
     def reset(self):
@@ -26,31 +34,15 @@ class FiniteMarkovProcess(ABC):
 
 class FiniteMarkovChain(FiniteMarkovProcess):
     #TODO: add key value for state indices,
-    def __init__(self, states: list, transition_matrix: np.array, initial_state=None, initial_state_dist=None):
-
-        super().__init__(states)
-
-        self.transition_matrix = transition_matrix
-        self.initial_state_distribution = initial_state_dist
-        self.initial_state = None
-
-        if initial_state is not None:
-            self.current_state = initial_state
-
-        elif self.initial_state_distribution:
-            self.current_state = self.rng.choice(self.n, 1, self.initial_state_distribution)[0] # returns np array
-
-        else:
-            self.current_state = self.rng.choice(self.n, 1)[0]
-
+    def __init__(self, states: list, trans_kernel: np.array, initial_state=None, initial_state_dist=None):
+        super().__init__(states, trans_kernel, initial_state, initial_state_dist)
 
     def reset(self):
         if self.initial_state is not None:
             self.current_state = self.initial_state
 
-        elif self.initial_state_distribution:
-            self.current_state = self.rng.choice(self.n, 1, self.initial_state_distribution)[0] # returns np array
-
+        elif self.initial_dist:
+            self.current_state = self.rng.choice(self.n, 1, self.initial_dist)[0] # returns np array even for j 1
         else:
             self.current_state = self.rng.choice(self.n, 1)[0]
 
@@ -58,31 +50,27 @@ class FiniteMarkovChain(FiniteMarkovProcess):
 
     def get_next_state_dist(self):
         # just index the matrix to get vector of probabilities
-        return self.transition_matrix[self.current_state]
+        return self.trans_kernel[self.current_state]
 
     def step(self):
 
         dist = self.get_next_state_dist()
 
-        #numpy random choice based on indexed dist
+        #numpy random choice based on prob vector got by indexing matrix
         self.current_state = self.rng.choice(self.n, 1, dist)[0]
 
         return self.current_state
 
 class FiniteMarkovRewards(FiniteMarkovChain):
-    def __init__(self, states: list, transition_matrix: np.array, rewards: list, initial_state=None, initial_state_dist=None):
-        super(FiniteMarkovRewards, self).__init__(states, transition_matrix, initial_state, initial_state_dist)
+    def __init__(self, states: list, trans_kernel: np.array, rewards: np.array, initial_state=None, initial_state_dist=None):
+        super().__init__(states, trans_kernel, initial_state, initial_state_dist)
 
         self.rewards = rewards
 
     def exp_rewards(self, state=None):
         # weighted sums of states' rewards, weighted by prob
         if state is not None:
-            print("TRANS. MATRIX")
-            print(self.transition_matrix)
-            print(f"TRANSITION DIST GIVEN state={state}")
-            print(self.transition_matrix[state])
-            dist = self.transition_matrix[state]
+            dist = self.trans_kernel[state]
 
             rewards = self.rewards
             print("REWARDS")
@@ -92,9 +80,7 @@ class FiniteMarkovRewards(FiniteMarkovChain):
             return dist @ rewards
 
         else:
-
-            dist = self.transition_matrix[self.current_state]
-
+            dist = self.trans_kernel[self.current_state]
             rewards = self.rewards
 
             # first ever @ operator usage lets go (dot product np)
@@ -135,5 +121,9 @@ class FiniteMarkovRewards(FiniteMarkovChain):
         return v
 
 
+class FiniteMarkovDecisionProcess(FiniteMarkovProcess):
+    def __init__(self, states: list, transition_matrix: np.array, rewards: list, initial_state=None,
+                 initial_state_dist=None):
+        super(FiniteMarkovDecisionProcess, self).__init__(states, transition_matrix, initial_state, initial_state_dist)
 
-
+        self.rewards = rewards
